af911cfd5daedd6749d4e34398727d0b6e20e07f
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2024-07-17 14:36:38 +0100

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# code-github-api-package
-A Python package used to interact with the GitHub RESTful API
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='github-api-toolkit',
+    version='0.1.0',
+    description='A toolkit for interacting with the GitHub API',
+    url='https://github.com/ONS-Innovation/code-github-api-package',
+    author='Kieran Pritchard',
+    author_email='kieran.pritchard@ons.gov.uk',
+    license='MIT',
+    packages=['github_api_toolkit'],
+    zip_safe=False
+)
